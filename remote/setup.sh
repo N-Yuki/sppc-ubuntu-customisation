0fb3288ed73c12cb45294b95433fcc19097d96b0
Yuki Osada <yuki.osada@research.uwa.edu.au>
2016-08-21 13:19:11 +0800

    Remove timezone setting from remote setup

--- remote/setup.sh
+++ remote/setup.sh
@@ -5,10 +5,6 @@ cat << EOF_FIREPREFS > /usr/lib/firefox/defaults/pref/all.corp.js
 lockPref("browser.startup.homepage","http://dmjappprd01.sit.auckland.ac.nz/domjudge/team/");
 EOF_FIREPREFS
 
-# Set timezone to Australia/Perth - Modify as necessary
-echo "Pacific/Auckland" | tee /etc/timezone
-dpkg-reconfigure --frontend noninteractive tzdata
-
 # Remove sudo access
 rm -fr /etc/sudoers.d
 sed -r -i '/%admin|%sudo/ s/^/#/' /etc/sudoers
