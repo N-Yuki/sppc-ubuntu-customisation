5bc2751852debeaac12a8ab3aff969b57f5072e5
Yuki Osada <yuki.osada@research.uwa.edu.au>
2016-09-07 13:32:57 +0800

    Readd the IPTABLES

--- remote/setup.sh
+++ remote/setup.sh
@@ -6,7 +6,18 @@ lockPref("browser.startup.homepage","http://dmjappprd01.sit.auckland.ac.nz/domju
 EOF_FIREPREFS
 
 # Setup IPTABLES to block all internet traffic, except the DOMJudge servers and IP printers
-
+iptables -F
+iptables -P INPUT DROP
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT DROP
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p udp -m udp --dport 53 -j ACCEPT
+iptables -A OUTPUT -d 132.181.7.114/32 -p tcp -m tcp --dport 80 -j ACCEPT
+iptables -A OUTPUT -d 130.216.8.43/32 -p tcp -m tcp --dport 80 -j ACCEPT
+iptables -A OUTPUT -p udp -m udp --dport 631 -j ACCEPT
+iptables -A OUTPUT -p tcp -m tcp --dport 631 -j ACCEPT
 
 # Remove sudo access
 rm -fr /etc/sudoers.d
