49cf7ab2be5121b53ea38b1a4dc28cc481af2c5b
Yuki <yuuki.k.osada@gmail.com>
2016-10-05 19:33:15 +0800

    Update DOMJudge server/backup URL/IP

--- remote/setup.sh
+++ remote/setup.sh
@@ -2,7 +2,7 @@
 
 # Set Firefox homepage to http://domserver.cosc.canterbury.ac.nz/domjudge/team/
 cat << EOF_FIREPREFS > /usr/lib/firefox/defaults/pref/all.corp.js
-lockPref("browser.startup.homepage","http://dmjappprd01.sit.auckland.ac.nz/domjudge/team/");
+lockPref("browser.startup.homepage","http://domserver.cosc.canterbury.ac.nz/public/");
 EOF_FIREPREFS
 
 # Setup IPTABLES to block all internet traffic, except the DOMJudge servers and IP printers
@@ -15,7 +15,8 @@ iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p udp -m udp --dport 53 -j ACCEPT
 iptables -A OUTPUT -d 132.181.7.114/32 -p tcp -m tcp --dport 80 -j ACCEPT
-iptables -A OUTPUT -d 130.216.8.43/32 -p tcp -m tcp --dport 80 -j ACCEPT
+iptables -A OUTPUT -d 60.241.98.115/32 -p tcp -m tcp --dport 80 -j ACCEPT
+iptables -A OUTPUT -d 130.194.22.140/32 -p tcp -m tcp --dport 80 -j ACCEPT
 iptables -A OUTPUT -p udp -m udp --dport 631 -j ACCEPT
 iptables -A OUTPUT -p tcp -m tcp --dport 631 -j ACCEPT
 
